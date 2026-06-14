a9a7a817a59cfe6031362e382c74f1db6ba7899b
Shreya Goyal <shreyabvm.7738@gmail.com>
2026-06-15 01:44:30 +0530

    Create ReverseWordInAString.cpp

---
+++ Week_2/Day_4/ReverseWordInAString.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n=s.size();
+        int i=0;
+
+        while(i<n){
+            int j=i;
+
+            while(j<n && s[j]!=' ') j++;
+
+            reverse(s.begin()+i,s.begin()+j);
+
+            i=j+1;
+        }
+
+        return s;
+    }
+};
